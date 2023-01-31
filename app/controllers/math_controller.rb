a7ee28c683c9b5590859cd904668f743c4f4d331
annenyc <annelau.nyc@gmail.com>
2023-01-31 09:51:50 +0000

    anne-omnicalc2-final commit

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,5 +1,19 @@
 class MathController < ApplicationController
 
+  def addition_results
+
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+
+    @result = @first + @second
+  
+    render({ :template => "math_templates/addition.html.erb" })
+  end 
+  
+  def addition_form
+    render({ :template => "math_templates/addition_form.html.erb" })
+  end
+
   def subtract_results
   #params is {"first_num"=>"42", "second_num"=>"16"}
 
@@ -17,4 +31,29 @@ class MathController < ApplicationController
     render({ :template => "math_templates/minus_form.html.erb" })
   end
   
+  def multiplication_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+
+    @result = @first * @second
+    render({ :template => "math_templates/multiplication.html.erb" })
+  end
+
+  def multiplication_form
+    render({ :template => "math_templates/multiplication_form.html.erb" })
+  end
+
+  def division_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+
+    @result = @first / @second
+    
+    render({ :template => "math_templates/divide.html.erb" })
+  end
+
+  def division_form
+    render({ :template => "math_templates/division_form.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
 get("/wizard_subtract", { :controller => "math", :action => "subtract_results" })
 get("/subtract", { :controller => "math", :action => "subtract_form" })
 
-#get("/wizard_add", { :controller => "math", :action => "addition_form" })
-#get("/addition_results", { :controller => "math", :action => "addition_results" })
+get("/wizard_add", { :controller => "math", :action => "addition_results" })
+get("/add", { :controller => "math", :action => "addition_form" })
 
-#get("/wizard_multiply", { :controller => "math", :action => "multiplication_form" })
-#get("/muliplication_results", { :controller => "math", :action => "multiplication_results" })
+get("/wizard_multiply", { :controller => "math", :action => "multiplication_results" })
+get("/multiply", { :controller => "math", :action => "multiplication_form" })
 
-#get("/wizard_divide", { :controller => "math", :action => "division_form" })
-#get("/division_results", { :controller => "math", :action => "division_results" })
+get("/wizard_divide", { :controller => "math", :action => "division_results" })
+get("/divide", { :controller => "math", :action => "division_form" })
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
